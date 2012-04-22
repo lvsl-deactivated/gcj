858cc27d0f0255de6678002f1df71273d090ae77
Leo Vassilyev <chin@shaftsoft.ru>
2012-04-22 21:36:43 +0600

    File Fix-it

---
+++ file_fix_it/main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+def count_mkdirs(old, new):
+    old = sorted(old, key=lambda d: d.count('/'))
+    new = sorted(new, key=lambda d: d.count('/'))
+
+    G = {}
+
+    for path in old:
+        g = G
+        for d in path[1:].split('/'):
+            if d not in g:
+                g[d] = {}
+            g = g[d]
+
+    count = 0
+    for path in new:
+        g = G
+        for d in path[1:].split('/'):
+            if d not in g:
+                g[d] = {}
+                count += 1
+            g = g[d]
+
+    return count
+
+def main():
+    T = int(raw_input())
+    for i in range(T):
+        N, M = map(int, raw_input().split())
+        old_dirs = []
+        for _ in xrange(N):
+            old_dirs.append(raw_input())
+        new_dirs = []
+        for _ in xrange(M):
+            new_dirs.append(raw_input())
+        print "Case #%s: %s" % (i+1, count_mkdirs(old_dirs, new_dirs))
+
+
+if __name__ == "__main__":
+    main()
